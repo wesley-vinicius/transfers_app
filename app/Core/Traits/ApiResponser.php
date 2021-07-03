43da063f79eb73f4452ff3b9d9a56bef4e2dbc02
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-07-03 16:02:12 -0300

    feat(core):  trait api response

---
+++ app/Core/Traits/ApiResponser.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Core\Traits;
+
+use Symfony\Component\HttpFoundation\Response;
+
+/*
+|--------------------------------------------------------------------------
+| Api Responser Trait
+|--------------------------------------------------------------------------
+|
+| This trait will be used for any response we sent to clients.
+|
+*/
+
+trait ApiResponser
+{
+	/**
+	 * Return a success JSON response.
+	 *
+	 * @param  array|string  $data
+	 * @param  string  $message
+	 * @param  int|null  $code
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	protected function success($data, string $message = null, int $code = Response::HTTP_OK)
+	{
+		return response()->json([
+			'message' => $message,
+			'data' => $data
+		], $code);
+	}
+
+	/**
+	 * Return an error JSON response.
+	 *
+	 * @param  string  $message
+	 * @param  int  $code
+	 * @param  array|string|null  $data
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	protected function error(string $message = null, int $code, $data = null)
+	{
+		return response()->json([
+			'message' => $message,
+			'data' => $data
+		], $code);
+	}
+}
