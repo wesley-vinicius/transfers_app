a884d720cb3918b36d8759796c89e9f6f57d28f6
Wesley Vinicius <viweesleyy9923@gmail.com>
2021-07-04 16:39:22 -0300

    feat(transaction):  transaction request

---
+++ app/Domain/Transaction/Http/Requests/TransactionRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Domain\Transaction\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class TransactionRequest extends FormRequest
+{
+
+    protected $stopOnFirstFailure = true;
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "payer" => ['bail', 'required', 'integer', 'exists:users,id'],
+            "payee" => ['bail', 'required', 'integer', 'different:payer', 'exists:users,id'],
+            "value" => ['required', 'gt:0', 'numeric', 'min:1'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'value.min' => 'The transfer amount must be greater than zero',
+            'payee.different' => 'The transfer cannot be made to the same user',
+            'payer.exists' => 'There is no payment with this id',
+            'payee.exists' => 'There is no beneficiary with this id',
+        ];
+    }
+}
---
+++ tests/Unit/Domain/Transaction/Http/Requests/TransactionRequestTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit\Domain\Transaction\Http\Requests;
+
+use App\Domain\Transaction\Http\Requests\TransactionRequest;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TransactionRequestTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testReturnArrayWithValidationsToCreateTransaction()
+    {
+        $rulesUserCreate = [
+            "payer" => ['bail', 'required', 'integer', 'exists:users,id'],
+            "payee" => ['bail', 'required', 'integer', 'different:payer', 'exists:users,id'],
+            "value" => ['required', 'gt:0', 'numeric', 'min:1']
+        ];
+
+        $userCreateRequest = new TransactionRequest();
+
+        $this->assertEquals($rulesUserCreate, $userCreateRequest->rules());
+    }
+
+    public function testAuthorizeTrue()
+    {
+        $userCreateRequest = new TransactionRequest();
+
+        $this->assertTrue($userCreateRequest->authorize());
+    }
+}
